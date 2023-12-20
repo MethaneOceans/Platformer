3bba40eddc4f529f5334356cd9c87b7065ea9c0b
MethaneOceans <t-velema@outlook.com>
2023-12-20 20:05:49 +0100

    Player collisions "fixed"
    Needs another redo without using MoveUntilCollision but that's lower priority right now.

--- GXPEngine/Player.cs
+++ GXPEngine/Player.cs
@@ -23,7 +23,8 @@ public class Player : Actor
 
     public void Update()
     {
-        // Doing this in the constructor proves to just not work because it's dependent on values that are yet to be defined at runtime
+        // Doing this in the constructor proves to just not work because it's dependent on values that are yet to be initialized
+        // #GXPengine things!
         if (!initialized)
         {
             List<GameObject> list = this.parent.GetChildren();
@@ -45,6 +46,7 @@ public class Player : Actor
 
         vY += 0.15f;
 
+        // Crap still causes getting stuck
         currentCollision = MoveUntilCollision(vX, 0);
         if (currentCollision != null)
         {
@@ -58,11 +60,32 @@ public class Player : Actor
             vX *= 0.9f;
             if (currentCollision.normal.y < 0)
             {
+                // This movement change is a very sketchy fix to the player getting stuck on the floor sometimes
+                // I want to fix it but I don't think I'll have the energy/brainpower for now to do that
+                y += currentCollision.normal.y * 0.001f;
                 canJump = true;
             }
         }
         currentCollision = null;
-        
+
+        // Move player for a total of vX and vY taking in consideration collisions
+        // Move vY
+        // If player collides it's with a ceiling or floor
+        // vX *= 0.9f
+        // vY = 0.0f
+        // If normal < 0 the player is on the ground
+        // canJump = true
+        // currentCollision = null
+        // Move vX
+        // If player collides it's with a wall
+        // vX = 0.0f
+        // In some weird cases however the player collides with the floor causing the player to not move left or right
+        // If normal.x < 0.1f && normal.x > -0.1f
+        // Just move?
+
+
+
+
     }
 
     private void handleInput()
@@ -83,6 +106,7 @@ public class Player : Actor
         }
 
         // "Smooth" camera movement
-        playerCamera.SetXY(playerCamera.x + (playerCamera.x - x) / -10f, playerCamera.y + (playerCamera.y - y) / -10f);
+        // ngl feels kinda nice to me
+        playerCamera.SetXY(playerCamera.x + (playerCamera.x - x) / -10f, playerCamera.y + (playerCamera.y - y) / -10f); 
     }
 }
